469b558ddfa51176fa93751496c3cb1d35f6a020
cxan <c.p.xanten@gmail.com>
2019-10-22 09:03:45 +0200

    started exercise 2

--- 01/uebung_01.R
+++ 01/uebung_01.R
@@ -1,3 +1,5 @@
+library(fma)
+
 # 1)
 
 sonnenflecken <- read.table('sonnenflecken.txt', header = TRUE)
@@ -34,3 +36,9 @@ plot(stromproduktion$Datum, stromproduktion$log_Strom_noTrend)
 lines(stromproduktion$Datum, stromproduktion$log_Strom_noTrend)
 
 acf(stromproduktion$log_Strom_noTrend)
+
+acf(creditspreads$CDSSpreads)
+
+# 2)
+
+plot(elec)
